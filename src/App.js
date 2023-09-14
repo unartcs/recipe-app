25350b7200349f1acb891c55b25542d4e41d6b01
unartcs <unartcs@gmail.com>
2023-09-14 12:28:38 +0200

    fixed links hopefully

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import Search from "./components/Search";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Search />
         <Category />
         <Pages />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
