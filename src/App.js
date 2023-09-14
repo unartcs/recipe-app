e880f9e5c79d9844b1cf7e57d49ef2a8d0d6a96b
unartcs <unartcs@gmail.com>
2023-09-14 12:37:19 +0200

    fixed links

--- src/App.js
+++ src/App.js
@@ -2,15 +2,12 @@ import "./App.css";
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import Search from "./components/Search";
-import { HashRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <HashRouter>
         <Search />
         <Category />
         <Pages />
-      </HashRouter>
     </div>
   );
 }
